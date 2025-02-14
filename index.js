a58f91bb7ef1511721bf9485057f6eb021cfcacd
MilesLK <bricefrancietta@hotmail.com>
2025-02-14 13:19:54 +0100

    refonte du design, on part sur quelques choses de plus moderne

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let lastScrollTop = 0;
+const navbar = document.querySelector(".navbar");
+
 document.addEventListener("DOMContentLoaded", () => {
     const burger = document.getElementById("burger");
     const navLinks = document.getElementById("nav-links");
@@ -5,4 +8,15 @@ document.addEventListener("DOMContentLoaded", () => {
     burger.addEventListener("click", () => {
         navLinks.classList.toggle("show");
     })
+});
+
+window.addEventListener("scroll", function () {
+    let scrollTop = this.window.scrollY || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+        navbar.style.top = "-100px";
+    } else {
+        navbar.style.top = "0"
+    }
+    lastScrollTop = scrollTop
 })
+
